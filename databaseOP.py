9f46bed9e1c8ac130d6ecf383da77c374d96202f
Gurpreet Singh <gurpreets0610@gmail.com>
2020-04-11 20:34:33 +0530

    bhasad2

--- databaseOP.py
+++ databaseOP.py
@@ -1,12 +1,50 @@
 import sqlite3
+import json
+
+def faculty_info_by_name(faculty_info_category,faculty_first_name,faculty_last_name):
+    full_name =faculty_first_name + " " +faculty_last_name
+    con = sqlite3.connect('data.sqlite')
+    cursorObj = con.cursor()
+    cursorObj.execute("SELECT {} FROM facultyDetails WHERE Name = '{}';".format(faculty_info_category,full_name))
+    rows = cursorObj.fetchall()
+    if(faculty_info_category=="Description"):
+        speech_response=rows[0][0]
+    elif(faculty_info_category=="Room"):
+        speech_response=full_name+" is usually present in room " + rows[0][0]
+    elif(faculty_info_category=="Expertise"):
+        speech_response=full_name+" is specialized in " + rows[0][0]
+    elif(faculty_info_category=="Experience"):
+        speech_response=full_name+" is having an experience" + rows[0][0]
+    elif(faculty_info_category=="Designation"):
+        speech_response=full_name+" is " + rows[0][0]
+    elif(faculty_info_category=="Department"):
+        speech_response=full_name+" belongs to " + rows[0][0] + " department"
+    
+    display_response =[["Name",faculty_info_category],[full_name,rows[0][0]]]
+    
+    
+    return {"fulfillmentText": speech_response,
+                "fulfillmentMessages": [
+                    {
+                    "text": {
+                        "text": [ json.dumps(display_response)
+                        
+                        ]        }
+                    }
+                ]
+                }
+    
+
+    
+    
+
 
 def faculty_information(name):
     con = sqlite3.connect('data.sqlite')
     cursorObj = con.cursor()
-    cursorObj.execute("SELECT Description FROM facultyProfile WHERE Name = '{}';".format(name))
+    cursorObj.execute("SELECT Description FROM facultyDetails WHERE Name = '{}';".format(name))
     rows = cursorObj.fetchall()
     return {'fulfillmentText': rows[0][0]}
-        
 def admissionBPIT(departments):
     rsp=""
     if(departments in ["Computer Science and Engineering", "Information Technology", "Electronics and Communication Engineering", "Electrical and Electronics Engineering" , "Bachelor of Technology"]):
@@ -26,3 +64,4 @@ def bpit_infrastructure_facility(infrastruture_facilities):
     return {'fulfillmentText': rows[0][0]}
 
 # print(bpit_infrastructure_facility("class"))
+faculty_info_by_name("Description","Dr Bhawna","Suri")
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def results(req):
     elif(action=="root_bpit.bpit.infrastructure_facility"):
         response=bpit_infrastructure_facility(parameters.get('infrastruture_facilities'))
     elif(action=="root_faculty.faculty.faculty_info_by_name"):
+        response=faculty_info_by_name(parameters.get('faculty_info_category'),
+                                      parameters.get('faculty_first_name'),parameters.get('faculty_last_name'))
         pass
     # return a fulfillment response
     return response
